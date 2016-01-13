2748ea81891f7b27dfdce498b41d31c6a3c2d176
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-13 10:09:04 +0700

    Use SSl and the Puma webserver in production

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rb-readline'
 
 gem 'table_print'
 
+gem 'puma', '2.11.1'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
