6f03755b93a309d416dc9c609aa97bb924db8635
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-15 17:50:48 +0700

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   host = 'vuducluan-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
     :authentication => :plain,
     :user_name      => 'nanofresh.vn@gmail.com',
