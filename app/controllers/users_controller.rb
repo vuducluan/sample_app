3a6a9770c68a175fa8bc6bc1a1bd3c1b17e3ae0e
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-18 09:34:09 +0700

    don't use user filter activated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,15 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    # @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
     # Filller only users actived
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])    
+    #@users = User.where(activated: FILL_IN).paginate(page: params[:page])    
   end
 
   def show
   	@user = User.find(params[:id])
     # redirect
-    redirect_to root_url and return unless FILL_IN    
+    # redirect_to root_url and return unless FILL_IN    
   end
 
   def new
--- app/models/user.rb
+++ app/models/user.rb
@@ -49,9 +49,9 @@ class User < ActiveRecord::Base
 
   #Activates an account.
   def activate
-    # update_attribute(:activated, true)
-    # update_attribute(:activated_at, Time.zone.now)
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)    
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+    # update_columns(activated: FILL_IN, activated_at: FILL_IN)    
   end
 
   # Sends activation email.
@@ -61,9 +61,9 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    # update_attribute(:reset_digest,  User.digest(reset_token))
-    # update_attribute(:reset_sent_at, Time.zone.now)
-    update_columns(reset_digest: FILL_IN,
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+    # update_columns(reset_digest: FILL_IN,
                    reset_sent_at: FILL_IN)
   end
 
