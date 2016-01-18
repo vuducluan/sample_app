a0a35fc2cef8abe9de2cc280323f96de9e58f6ca
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-18 12:56:57 +0700

    change account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-    # redirect
+    @microposts = @user.microposts.paginate(page: params[:page])
     # redirect_to root_url and return unless FILL_IN    
   end
 
@@ -57,14 +57,7 @@ class UsersController < ApplicationController
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
     # Before filters
-    # Confirms a logged-in user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url 
-      end
-    end
+    
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
