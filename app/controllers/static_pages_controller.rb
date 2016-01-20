0af1e4c22dab6bd8a6dee4f15271f3426b771315
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-20 13:16:39 +0700

    modifier home page, all guest reading micropost and comment

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
   	if logged_in?
-  	  @micropost  = current_user.microposts.build if logged_in?
-  	  @feed_items = current_user.feed.paginate(page: params[:page])
-  	end
+  	  @micropost  = current_user.microposts.build
+  	  @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10)
+    end
+      @microposts = Micropost.all
+      @feed_items = @microposts.paginate(page: params[:page], per_page: 10)
   end
 
   def help
